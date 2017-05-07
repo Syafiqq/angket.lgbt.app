257c263caa44eafa87660b106006ff42df4c19d1
Syafiqq <syafiq.rezpector@gmail.com>
2017-05-07 21:16:50 +0700

    Add flatify route

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,22 +19,31 @@ class Welcome extends CI_Controller
      * map to /index.php/welcome/<method_name>
      * @see https://codeigniter.com/user_guide/general/urls.html
      */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+
     public function index()
     {
+
         $this->load->view('welcome_message');
     }
 
     public function boilerplate()
     {
-        $this->load->helper('url');
-
         $this->load->view('boilerplate');
     }
 
     public function bootstrap()
     {
-        $this->load->helper('url');
-
         $this->load->view('bootstrap');
+    }
+
+    public function flatify()
+    {
+        $this->load->view('flatify');
     }
 }
