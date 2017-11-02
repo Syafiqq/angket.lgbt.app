cea119832d0df15445ca58283460c5eca92bd4c9
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-11-02 12:54:17 +0700

    Remove watcher

--- gulpfile.js
+++ gulpfile.js
@@ -23,24 +23,3 @@ gulp.task('minify-css', function () {
         .pipe(gulp.dest('./assets/'));
 });
 
-gulp.task('watch-minify-js', function () {
-    return watch(['./raw/**/*.js', '!./raw/**/*.min.js'], function (cb) {
-        pump([
-                gulp.src(['./raw/**/*.js', '!./raw/**/*.min.js'], {dot: true, base: './raw/'})
-                    .pipe(rename({suffix: ".min", extname: ".js"})),
-                uglify(),
-                gulp.dest('./assets/')
-            ],
-            cb
-        );
-    });
-});
-
-gulp.task('watch-minify-css', function () {
-    return watch(['./raw/**/*.css', '!./raw/**/*.min.css'], function () {
-        return gulp.src(['./raw/**/*.css', '!./raw/**/*.min.css'], {dot: true, base: './raw/'})
-            .pipe(cleanCSS({compatibility: 'ie8', rebase: false}))
-            .pipe(rename({suffix: ".min", extname: ".css"}))
-            .pipe(gulp.dest('./assets/'));
-    });
-});
